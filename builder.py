6bdac20660657a211c31574c33261543d42247e1
Julien Acroute <julien.acroute@camptocamp.com>
2023-09-18 09:47:08 +0200

    feat: read event from pod logs

--- builder.py
+++ builder.py
@@ -4,10 +4,14 @@ import signal
 import json
 import jinja2
 import argparse
+import time
 from collections import defaultdict
 from kubernetes import client, config
 from kubernetes.client.exceptions import ApiException
 from pprint import pprint
+from queue import SimpleQueue, Empty
+from threading import Thread, current_thread, Lock
+
 
 def parse_lines_eol_terminator(filename):
   """
@@ -64,14 +68,14 @@ def extract_workload_prefix(ns, s):
   """
   match = re.match(r'^(.*?)-\d+[a-zA-Z0-9-]+$', s)
   if match:
-    return match.group(1)
+    return (ns, 'guess-Deployment', match.group(1))
   else:
     # this is very try an error, but we are guessing now workload is a deamon set
     # we assume pattern workload-<pod-id>
 
       match = re.match(r'^(.*?)-[a-zA-Z0-9]+$', s)
       if match:
-        return match.group(1)
+        return (ns, 'guess-Deamonset', match.group(1))
       else:
         print(f"Error matching: {s}")
         return None
@@ -106,89 +110,145 @@ class EventProcessExec:
 class EventProcessExit(EventProcessExec):
   pass
 
-class PodNotfound(Exception):
-  pass
+class BufferedDictSet():
 
-class Analyzer:
+  # Dictionary of Set with a modification Buffer
+  # to be able to make batch modifications.
+  #
+  # d = BufferedDictSet()
+  # d.add("a", 2)
+  # d.add("b", 3) 2 pending modifications
+  # d.add("a", 3) 3 pending modifications
+  # write_to_disk(d.getDict()) --> {"a": {2, 3}, "b": {3}}
+  # d.flush() no more pending modifications
 
   def __init__(self):
-    self.event_counter = dict()
-    self.ns_ls = []
-    self.ps_ls = []
-    self.pod_to_workload = dict()
+    self.written = dict()
+    self.to_write = dict()
 
-    # Load K8S configuration
-    config.load_kube_config()
+  def __str__(self):
+    res = "Written:\n"
+    for wl, bins in self.written.items():
+      res += "%s: %s\n" % (wl, ",".join(bins))
+    res += "To write:\n"
+    for wl, bins in self.to_write.items():
+      res += "%s: %s\n" % (wl, ",".join(bins))
+    return res
+
+  # set a value in the 'buffer' if this is not already present
+  def add(self, key, value):
+    print("Adding %s for %s:\n%s" % (value, key, self))
+    if key not in self.written or value not in self.written[key]:
+      if key in self.to_write:
+        self.to_write[key].add(value)
+      else:
+        self.to_write[key] = {value}
+
+  def modificationCount(self):
+    return len(self.to_write)
+
+  def getDict(self):
+    # deep merge !
+    res = dict()
+    for key in self.written:
+      res[key] = self.written[key]
+    for key in self.to_write:
+      if key in res:
+        res[key].update(self.to_write[key])
+      else:
+        res[key] = self.to_write[key]
+    return res
+
+  def flush(self):
+    self.written = self.getDict()
+    self.to_write = dict()
+
+class PodNotfound(Exception):
+
+  def __init__(self, pod):
+    self.pod = pod
+
+  def __str__(self):
+    return "Pod not Found: %s" % self.pod
+
+class ReplicasetNotfound(Exception):
+
+  def __init__(self, rs):
+    self.rs = rs
+
+  def __str__(self):
+    return "Replicaset not Found: %s" % self.rs
+
+class NamespaceAnalyser:
+
+  def __init__(self, ns):
+    self.ns = ns
+    self.pod_to_workload = dict()
     self.v1 = client.CoreV1Api()
     self.appsv1 = client.AppsV1Api()
+    self.binaries = BufferedDictSet()
+    self.lock = Lock()
 
-  def getWorkload(self, ns, pod_id):
-    if pod_id in self.pod_to_workload:
-      return self.pod_to_workload[pod_id]
+  def getWorkload(self, pod):
+    if pod in self.pod_to_workload:
+      return self.pod_to_workload[pod]
     else:
-      # Retrieve Pod owner
-      try:
-        owner = self.getPodOwner(ns, pod_id)
-        print(owner)
-        if owner[1] == 'ReplicaSet':
-          return self.getRSOwner(ns, owner[2])
-      except PodNotfound as e:
-        # fallback to pod name parsing
-        return extract_workload_prefix(ns, pod_id)
+      owner = self.getPodOwner(pod)
+      if owner[0] == 'ReplicaSet':
+        return self.getRSOwner(owner[1])
 
-  def getPodOwner(self, ns, pod_name):
+  def getPodOwner(self, pod):
     try:
-      api_response = self.v1.read_namespaced_pod(pod_name, ns, pretty=True)
-      pprint(api_response)
+      api_response = self.v1.read_namespaced_pod(pod, self.ns, pretty=True)
+      #pprint(api_response)
       owner = api_response.metadata.owner_references[0]
-      return (ns, owner.kind, owner.name)
+      return (owner.kind, owner.name)
     except ApiException as e:
       if e.reason == 'Not Found':
-        raise PodNotfound()
+        raise PodNotfound(pod)
 
-  def getRSOwner(self, ns, rs_name):
+  def getRSOwner(self, rs):
     try:
-      api_response = self.appsv1.read_namespaced_replica_set(rs_name, ns, pretty=True)
-      pprint(api_response)
+      api_response = self.appsv1.read_namespaced_replica_set(rs, self.ns, pretty=True)
+      #pprint(api_response)
       owner = api_response.metadata.owner_references[0]
-      return (ns, owner.kind, owner.name)
+      return (owner.kind, owner.name)
     except ApiException as e:
-        print("Exception when calling AppsV1Api->read_namespaced_replica_set: %s\n" % e)
+      if e.reason == 'Not Found':
+        raise ReplicasetNotfound(rs)
 
-  def count(self, e:EventProcessExec):
-    if e in self.event_counter:
-      self.event_counter[e] += 1
-    else:
-      self.event_counter[e] = 1
+  def process(self, event):
+    #print("Searching workload for %s/%s" % (self.ns, event[1]))
+    wl = self.getWorkload(event[1])
+    #print("Workload for %s/%s is %s" % (self.ns, event[1], wl))
 
-  def process(self, d: dict):
-      e = None
+    with self.lock:
+      self.binaries.add("%s-%s" % (wl[0], wl[1]), event[2])
 
-      if "process_exec" in d:
-        e = EventProcessExec(
-           ns  = d["process_exec"]["process"]["pod"]["namespace"],
-           ctr = d["process_exec"]["process"]["pod"]["container"]["name"],
-           bin = d["process_exec"]["process"]["binary"],
-           wl  = self.getWorkload(d["process_exec"]["process"]["pod"]["namespace"],
-                                  d["process_exec"]["process"]["pod"]["name"]),
-        )
-      elif "process_exit" in d:
-        e = EventProcessExit(
-           ns  = d["process_exit"]["process"]["pod"]["namespace"],
-           ctr = d["process_exit"]["process"]["pod"]["container"]["name"],
-           bin = d["process_exit"]["process"]["binary"],
-           wl  = self.getWorkload(d["process_exit"]["process"]["pod"]["namespace"],
-                                  d["process_exit"]["process"]["pod"]["name"]),
-        )
+  def flush(self):
+     print("Flushing binaries for %s" % self.ns)
 
-      else:
-         raise NotImplementedError(f"unknown event type: {d}")
+     # Delete current configmap
+     with self.lock:
+       try:
+         self.v1.delete_namespaced_config_map("tetragon-binaries", self.ns)
+       except Exception as ex:
+         pass
 
-      self.count(e)
-      return e
+       # Create configmap
+       body = client.V1ConfigMap(
+         api_version="v1",
+         kind="ConfigMap",
+         metadata=client.V1ObjectMeta(name="tetragon-binaries"),
+         data={wl: json.dumps(list(value)) for (wl, value) in self.binaries.getDict().items()}
+       )
+       self.v1.create_namespaced_config_map(namespace=self.ns, body=body)
+       self.binaries.flush()
 
-  def print_stats(self):
-    print(self.event_counter)
+  def modificationCount(self):
+    with self.lock:
+      return self.binaries.modificationCount()
+
 
 def export_policy(events: list[EventProcessExec]) -> str:
   """
@@ -254,17 +314,67 @@ spec:
   return rendered
 
 
+def read_pod_log(pod, queue):
+  print("%s manage %s" % (current_thread().name, pod.metadata.name))
+  v1 = client.CoreV1Api()
+  print('In the thread !')
+  stream = v1.read_namespaced_pod_log(pod.metadata.name, pod.metadata.namespace, container="export-stdout", follow=True, _preload_content=False)
+  while True:
+    line = stream.readline()
+    if not line:
+      time.sleep(1)
+      continue
+
+    queue.put(json.loads(line.decode('utf-8')))
+
+def parse_event(e):
+
+    # Extract event type
+    eventType = list(e.keys())
+    eventType.remove('node_name')
+    eventType.remove('time')
+    eventType = eventType[0]
+
+    # Only use process_exec events
+    if eventType != "process_exec":
+      return None
+
+    # Extract metadata
+    ns = e[eventType]["process"]["pod"]["namespace"]
+    pod = e[eventType]["process"]["pod"]["name"]
+    bin = e[eventType]["process"]["binary"]
+    return (ns, pod, bin)
+
+class BackgroundFlush(Thread):
+
+  def __init__(self, analyzers):
+    self.analyzers = analyzers
+    super().__init__()
+
+  def run(self):
+    while True:
+      time.sleep(10)
+      for _, analyzer in self.analyzers.items():
+        if analyzer.modificationCount() > 0:
+          analyzer.flush()
+
+
 def main():
 
   # cli
   parser = argparse.ArgumentParser(description="Process input from stdin or a file.")
-  parser.add_argument('--file', type=str, help='path to input file')
+  mode = parser.add_mutually_exclusive_group()
+  mode.add_argument('--stream-from', type=str, help='stream from the tetragon pods, should contains a pod selector list: app.kubernetes.io/instance=tetragon,app.kubernetes.io/name=tetragon')
+  mode.add_argument('--file', type=str, help='path to input file')
   parser.add_argument('--eol-parser', type=str, help='use EOL parser instead of braces count based parser')
   parser.add_argument('--output', type=str, help='path to input file')
   args = parser.parse_args()
 
+  # Load K8S configuration
+  config.load_kube_config()
+
   # states
-  analyzer = Analyzer()
+  analyzers = dict()
   events = []
 
   def write_policies():
@@ -305,6 +415,44 @@ def main():
           print(f"Couldn't parse data from line number {line_number}.")
         if e not in events:
           events.append(e)
+  # read from pods logs
+  elif args.stream_from:
+    v1 = client.CoreV1Api()
+
+    try:
+      pod_list = v1.list_pod_for_all_namespaces(pretty=True, label_selector=args.stream_from)
+      #pprint(pod_list)
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
+
+    queue = SimpleQueue()
+    threads = []
+
+    for pod in pod_list.items:
+      t = Thread(name=pod.metadata.name, target=read_pod_log, args=(pod, queue))
+      t.start()
+      threads.append(t)
+
+    bg_sync = BackgroundFlush(analyzers)
+    bg_sync.start()
+
+    # read message from the queue
+    msg = None
+    while True:
+      #print("Waiting for message")
+      msg = queue.get(block=True)
+
+      #print("Process one message")
+      # Parse event
+      event = parse_event(msg)
+      if event:
+        if event[0] not in analyzers:
+          analyzers[event[0]] = NamespaceAnalyser(event[0])
+        analyzers[event[0]].process(event)
+        if analyzers[event[0]].modificationCount() > 10:
+          analyzers[event[0]].flush()
+
+
   # else read from stdin
   else:
     for line in sys.stdin:
